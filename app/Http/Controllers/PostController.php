d234bf500599f78c7d91daf159cf4db5486c034b
ShaoXi <shaoxi9733@gmail.com>
2024-10-17 09:26:42 +0800

    Creating PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,5 +8,32 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-  
+  public function store(Request $request){
+
+    $post = new Post();
+    $image = new Image();
+
+    //Request to form validate
+    $request->validate([
+        'title'=> 'required',
+        'content'=>'required',
+        'image.*' =>'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    ]);
+
+    $post = Post::create([
+        'title'=> $request->title,
+        'content'=> $request->content,
+    ]);
+
+    if($request->hasFile('image')){
+        foreach ($request->file('image') as $file) {
+            $path = $imageFile->store('images','public');
+            Image::create([
+                'path' =>$path,
+                'post_id' => $post->id
+            ]);
+        }
+    }
+    return redirect()->route('posts.index');
+  }
 }
